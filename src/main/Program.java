4ae17db96de9316638c4bd30953ef472c0cff9d4
luis afonso <luisafonso2000@hotmail.com>
2019-12-04 23:59:48 -0300

    mudanças para apresentação

--- src/main/Program.java
+++ src/main/Program.java
@@ -12,12 +12,10 @@ public class Program {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        userRepository.registryUser("luisafonso2000@hotmail.com", "123");
+        userRepository.registryUser("luisafonso@email.com", "123");
         userRepository.registryUser("jo@email.com", "123");
         userRepository.registryUser("duda@email.com", "123");
         userRepository.registryUser("gaby@email.com", "123");
-        userRepository.registryUser("viviane@email.com", "123");
-        userRepository.registryUser("jose@email.com", "pikachu_fly");
 
         Menu menu = new MainMenu();
         menu.renderOptions();
